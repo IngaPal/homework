7202e56299f524b5ffa81be759a7b7632208fec0
IngaPal <IngaPUFFY@web.de>
2023-11-04 04:19:33 +0100

    make addition calculator

--- src/Home.java
+++ src/Home.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Home {
     public static void main(String[] args) {
-        System.out.println(" Hello world");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
     }
 }
